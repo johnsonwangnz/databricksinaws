aa4d463bc35a28ff4f3f6d744018967de7d69424
Johnson Wang <johnson.wang@newcrest.com.au>
2023-10-02 13:35:25 +1300

    remvoe unused providers

--- awsdatabricksunitycatalog/providers.tf
+++ awsdatabricksunitycatalog/providers.tf
@@ -14,23 +14,10 @@ provider "aws" {
   region = var.region
 }
 
-// initialize provider in "MWS" mode to provision new workspace
-#provider "databricks" {
-#  alias      = "mws"
-#  host       = "https://accounts.cloud.databricks.com"
-#  account_id = var.databricks_account_id   // like a shared account? HA from multiple email accounts
-#  username   = var.databricks_account_username
-#  #password   = var.databricks_account_password
-#  auth_type  = "basic"
-#}
+
 
 provider "databricks" {
   alias = "ws1"
   profile = "awsucdev"
 }
 
-#provider "databricks" {
-#  alias = "ws2"
-#  host  = "https://dbc-dc6a79a6-893f.cloud.databricks.com"
-#  token = var.pat_ws_2
-#}
